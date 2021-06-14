2793f5008c8e252e84a452204ed77a0eea7ff895
Pavel <zavod93@mail.ru>
2021-06-14 19:32:00 +0300

    create home work 3

--- Algorithms_and_data_structures/Algorithms_and_data_structures/Program.cs
+++ Algorithms_and_data_structures/Algorithms_and_data_structures/Program.cs
@@ -8,11 +8,7 @@ namespace Algorithms_and_data_structures
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World");
-            Console.ReadLine();
-        }
-        
+  
+
     }
 }
