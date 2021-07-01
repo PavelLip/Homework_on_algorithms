17eee827499ababdfca9f6ff62f6eef39c18087a
Pavel <zavod93@mail.ru>
2021-07-02 00:10:01 +0300

    Copmpleted fifth homework v 0.1

--- Algorithms_and_data_structures/Algorithms_and_data_structures/Program.cs
+++ Algorithms_and_data_structures/Algorithms_and_data_structures/Program.cs
@@ -10,9 +10,20 @@ namespace Algorithms_and_data_structures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            var tree = new Tree();
+            int countElement = 20;
+            var newNumber = new Random(17);
+
+            for (int i = 0; i < countElement; i++)
+            {
+                tree.AddItem(newNumber.Next(1, countElement));
+            }
+            var number = newNumber.Next(1, countElement);
+            tree.SearchBFS(number);
+            Console.WriteLine();
+            tree.SearchDFS(number);
+
             Console.ReadLine();
         }
-        
     }
 }
