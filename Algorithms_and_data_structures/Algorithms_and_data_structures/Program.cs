138ec3bc3b5ecdb8e89faed370b0069a298eb939
Pavel <zavod93@mail.ru>
2021-07-05 02:00:29 +0300

    copmleted seventh homework v1

--- Algorithms_and_data_structures/Algorithms_and_data_structures/Program.cs
+++ Algorithms_and_data_structures/Algorithms_and_data_structures/Program.cs
@@ -11,35 +11,9 @@ namespace Algorithms_and_data_structures
         const int N = 2000;
         const int M = 1000;
 
-        static void Print2(int n, int m, int[,] a)
-        {
-            int i, j;
-            for (i = 0; i < n; i++)
-            {
-                for (j = 0; j < m; j++)
-                    Console.Write(a[i, j]);
-                Console.Write("\r\n");
-            }
-        }
-
         static void Main(string[] args)
         {
-
-            //int i, j;
-            //for (j = 0; j < M; j++)
-            //    A[0, j] = 1; // Первая строка заполнена единицами
-            //for (i = 1; i < N; i++)
-            //{
-            //    A[i, 0] = 1;
-            //    for (j = 1; j < M; j++)
-            //        A[i, j] = A[i, j - 1] + A[i - 1, j];
-            //}
-
-            //Print2(N, M, A);
-            //Console.WriteLine(A[N-1,M-1]);
-
             Console.WriteLine($"Колличество путей для поля {N}x{M} равняется: {CountPuth(N, M)}");
-
             Console.ReadLine();
         }
 
@@ -60,11 +34,6 @@ namespace Algorithms_and_data_structures
                 }
             }
             return array[n-1, m-1];
-
         }
-
     }
-
-   
-
 }
