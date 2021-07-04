6c40842d079b12fa78062a0673f1c99c089618a9
Pavel <zavod93@mail.ru>
2021-07-04 15:19:49 +0300

    Completed sixth homework v 0.1

---
+++ Algorithms_and_data_structures/Algorithms_and_data_structures/Graph.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_and_data_structures
+{
+    public interface IGraph
+    {
+        void SearchDFS(int startPosition);
+        void SearchBFS(int startPosition); 
+    }
+
+    class Graph : IGraph
+    {
+        public int[,] AdjacencyMatrix { get; set; }
+        public void SearchBFS(int startPosition)// поиск в ширину
+        {
+            Console.WriteLine("Запущен поиск в ширину");
+            var queue = new Queue<int>();
+            Console.WriteLine($"Положили в очередь начальную позицию {startPosition}");
+            queue.Enqueue(startPosition);
+            int graphNode;
+            bool[] node = new bool[AdjacencyMatrix.GetLength(0)]; //массив для понимания в каких узлах мы уже были
+            node[startPosition] = true; 
+            //для удобного вывода обхода графа
+            int[] bypassOrder = new int[AdjacencyMatrix.GetLength(0)];
+            int count = 0;
+            bypassOrder[count] = startPosition;
+            do
+            {
+                graphNode = queue.Dequeue();
+                Console.WriteLine($"Забрали из очереди узел {graphNode}");
+                for (int j = 0; j < AdjacencyMatrix.GetLength(0); j++)
+                {
+                    if (AdjacencyMatrix[graphNode, j] != 0)
+                    {
+                        if (node[j] == false)
+                        {
+                            queue.Enqueue(j);
+                            Console.WriteLine($"Положили в очередь узел {j}");
+                            node[j] = true;
+                            count += 1;
+                            bypassOrder[count] = j;
+                        }
+                    }
+                }
+            } while (queue.Count != 0);
+            Console.Write("Обход графа в ширину закончен, порядок обхода:");
+            foreach (int item in bypassOrder)
+            {
+                Console.Write($" {item}");
+            }
+            Console.WriteLine();
+        }
+
+        public void SearchDFS(int startPosition) // поиск в глубину
+        {
+            Console.WriteLine("Запущен поиск в глубину");
+            var stack = new Stack<int>();
+            stack.Push(startPosition);
+            int graphNode;
+            bool[] node = new bool[AdjacencyMatrix.GetLength(0)]; //массив для понимания в каких узлах мы уже были
+            //для удобного вывода обхода графа
+            int[] bypassOrder = new int[AdjacencyMatrix.GetLength(0)];
+            int count = 0;
+            bypassOrder[count] = startPosition;
+            do
+            {
+                graphNode = stack.Pop();
+                if (node[graphNode] == false) // проверка были ли мы уже в этом узле 
+                {
+                    node[graphNode] = true;
+                    bypassOrder[count] = graphNode;
+                    count += 1;
+                    Console.WriteLine($"Забрали из стека узел {graphNode}");
+                    for (int j = AdjacencyMatrix.GetLength(0) - 1; j >= 0; j--)
+                    {
+                        if (AdjacencyMatrix[graphNode, j] != 0)
+                                stack.Push(j);
+                    }
+                }
+            } while (stack.Count != 0);
+            Console.Write("Обход графа в глубину закончен, порядок обхода:");
+            foreach (int item in bypassOrder)
+            {
+                Console.Write($" {item}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Algorithms_and_data_structures/Algorithms_and_data_structures/Program.cs
+++ Algorithms_and_data_structures/Algorithms_and_data_structures/Program.cs
@@ -10,9 +10,26 @@ namespace Algorithms_and_data_structures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello ");
+            int [,] adjacencyMatrix = {
+                { 0,  3,  3,  6,  0,  11, 0,  0,  0,  0,  0},
+                { 3,  0,  0,  0,  2,  0,  0,  0,  0,  5,  0},
+                { 3,  0,  0,  0,  0,  0,  5,  11, 0,  0,  3},
+                { 6,  0,  0,  0,  0,  0,  0,  0,  5,  0,  5},
+                { 0,  2,  0,  0,  0,  0,  0,  5,  0,  9,  0},
+                { 11, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0},
+                { 0,  0,  5,  0,  0,  0,  0,  3,  0,  0,  0},
+                { 0,  0,  11, 0,  5,  0,  3,  0,  0,  0,  0},
+                { 0,  0,  0,  5,  0,  0,  0,  0,  0,  0,  0},
+                { 0,  5,  0,  0,  9,  0,  0,  0,  0,  0,  0},
+                { 0,  0,  3,  5,  0,  0,  0,  0,  0,  0,  0}
+            };
+
+            var graph = new Graph();
+            graph.AdjacencyMatrix = adjacencyMatrix;
+            graph.SearchBFS(0); // ожидаемый результат 0  1  2  3  5  4  9  6  7  10 8
+            graph.SearchDFS(0); // ожидаемый результат 0  1  4  7  2  6  10 3  8  9  5
+
             Console.ReadLine();
         }
-        
     }
 }
